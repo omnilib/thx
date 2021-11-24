4e959c2b08cf2b63a929626eaeff01ecb77e1a96
John Reese <john@noswap.com>
2021-11-23 20:30:12 -0800

    fix lint

--- thx/main.py
+++ thx/main.py
@@ -1,15 +1,16 @@
 # Copyright 2021 John Reese
 # Licensed under the MIT License
 
-from typing_extensions import Required
 import click
+
 from thx import __doc__, __version__
-from typing import Iterable
+
 
 @click.group(help=__doc__)
 @click.version_option(__version__, "--version", "-V")
 def main():
     pass
+
 
 @main.command("list")
 def list_commands():
@@ -17,4 +18,3 @@ def list_commands():
     List available commands and exit.
     """
     pass
-
