0dae25f768124ab58ef9e63d5d1fb13721fdb688
John Reese <john@noswap.com>
2021-11-28 13:55:27 -0800

    Use proactor event loop on Windows and < 3.8

--- thx/tests/__main__.py
+++ thx/tests/__main__.py
@@ -1,7 +1,12 @@
 # Copyright 2021 John Reese
 # Licensed under the MIT License
 
+import asyncio
+import sys
 import unittest
 
 if __name__ == "__main__":
+    if sys.platform == "win32" and sys.version_info < (3, 8):
+        loop = asyncio.ProactorEventLoop()
+        asyncio.set_event_loop(loop)
     unittest.main("thx.tests", verbosity=1)
